d2e9a59a106e2a4c2b715cfae21aef523a8b82ed
Adnan Jaberi <Tux.adn@gmail.com>
2023-02-26 14:07:38 +0300

    My second commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"ajaberi\"}}){id}}"}'
+curl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"ajaberi\"}}){id}}"}' | grep -oE '[0-9]+'
