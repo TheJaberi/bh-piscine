eff95a239b9f81a6cf523de1a3f9410a874db18d
Adnan Jaberi <tux.adn@gmail.com>
2023-02-28 15:41:29 +0300

    ye

--- printcomb2.go
+++ printcomb2.go
@@ -2,7 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func printcomb2() {
+func printComb2() {
 	for x := '0'; x <= '9'; x++ {
 		for y := '0'; y <= '9'; y++ {
 			for z := '0'; z <= '9'; z++ {
