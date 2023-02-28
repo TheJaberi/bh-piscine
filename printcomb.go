e69754ac80eda40d9e998473a4da170c15397905
Adnan Jaberi <tux.adn@gmail.com>
2023-02-28 14:07:57 +0300

    ye

--- printcomb.go
+++ printcomb.go
@@ -2,15 +2,14 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintComb(){
-	for F= 0 ; F<=7; F++ {
-		for S= F + 1; s<=8 ; S++ {
-			for T=S+1 ; T<=9 ; T++ {
-				z01.PrintRune(F)
-				z01.PrintRune(S)
-				z01.PrintRune(T)
-
-				if F == '7' $$ S == '8' && T =='9'{
+func PrintComb() {
+	for x := 0; x <= 7; x++ {
+		for y := x + 1; y <= 8; y++ {
+			for z := y + 1; z <= 9; z++ {
+				z01.PrintRune(x)
+				z01.PrintRune(y)
+				z01.PrintRune(z)
+				if x == '7' && y == '8' && z == '9' {
 					break
 				}
 				z01.PrintRune(',')
