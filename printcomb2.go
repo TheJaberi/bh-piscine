b6137ff965e36e72fc3be7729994bc5788f0d820
Adnan Jaberi <tux.adn@gmail.com>
2023-02-28 15:25:35 +0300

    ye

---
+++ printcomb2.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func printcomb2() {
+	for x := '0'; x <= '9'; x++ {
+		for y := '0'; y <= '9'; y++ {
+			for z := '0'; z <= '9'; z++ {
+				for a := '0'; a <= '9'; a++ {
+					if x != '0' || y != '0' || z != '0' || a != '0' {
+						z01.PrintRune(x)
+						z01.PrintRune(y)
+						z01.PrintRune(' ')
+						z01.PrintRune(z)
+						z01.PrintRune(a)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
