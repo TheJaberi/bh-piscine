9953028f4d4bd8b68d74cd6abcafc7550483e8ce
ajaberi <adnan.ajaberi@gmail.com>
2023-03-06 00:16:06 +0300

    ye

--- nrune.go
+++ nrune.go
@@ -1,6 +1,14 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	my_arr := []rune(s)
-	return (my_arr[(n - 1)])
+	my_c := 0
+	for index := range s {
+		my_c = index
+	}
+	if (my_c+1) >= n && n > 0 {
+		my_arr := []rune(s)
+		return (my_arr[n-1])
+	} else {
+		return 0
+	}
 }
