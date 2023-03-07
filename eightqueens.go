17e61a8a9953818af9d13625a7d955b5a3e70102
ajaberi <adnan.ajaberi@gmail.com>
2023-03-07 21:56:55 +0300

    ye

--- eightqueens.go
+++ eightqueens.go
@@ -1,56 +1,71 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
-var ans [8]rune
-var ban [9]bool
-var ans2 [9]int
+const size = 8
+
+var board [size][size]bool
+
+func goodDirection(x, y, vx, vy int) bool {
+	for 0 <= x && x < size &&
+		0 <= y && y < size {
+		if board[x][y] {
+			return false
+		}
+		x = x + vx
+		y = y + vy
+	}
+
+	return true
+}
+
+func goodSquare(x, y int) bool {
+	return goodDirection(x, y, +0, -1) &&
+		goodDirection(x, y, +1, -1) &&
+		goodDirection(x, y, +1, +0) &&
+		goodDirection(x, y, +1, +1) &&
+		goodDirection(x, y, +0, +1) &&
+		goodDirection(x, y, -1, +1) &&
+		goodDirection(x, y, -1, +0) &&
+		goodDirection(x, y, -1, -1)
+}
+
+func printQueens() {
+	x := 0
+	for x < size {
+		y := 0
+		for y < size {
+			if board[x][y] {
+				z01.PrintRune(rune(y) + '1')
+			}
+			y++
+		}
+		x++
+	}
+	z01.PrintRune('\n')
+}
+
+func tryX(x int) {
+	y := 0
+	for y < size {
+		if goodSquare(x, y) {
+
+			board[x][y] = true
+
+			if x == size-1 {
+				printQueens()
+			} else {
+				tryX(x + 1)
+			}
+
+			board[x][y] = false
+		}
+		y++
+	}
+}
 
 func EightQueens() {
-	ok := true
-	cnt := 0
-	for i := 1; i <= 8; i++ {
-		if ban[i] == false {
-			ok = false
-			//	break
-		} else {
-			cnt++
-		}
-	}
-
-	if ok == true {
-		for _, c := range ans {
-			z01.PrintRune(c)
-		}
-		z01.PrintRune('\n')
-
-		return
-	}
-	for i := '1'; i <= '8'; i++ {
-		cur := 0
-		for j := '1'; j <= i; j++ {
-			cur++
-		}
-		if ban[cur] == false {
-			put := true
-			for j := 1; j <= cnt; j++ {
-				if cur == ans2[j]-(cnt+1-j) || cur == ans2[j]+(cnt+1-j) {
-					put = false
-					break
-				} /*
-					fmt.Print(ans2[j])
-					fmt.Print(" ")*/
-			}
-			//	fmt.Println("    <<< THIS SHIT")
-			if put == true {
-				ban[cur] = true
-				ans[cnt] = i
-				ans2[cnt+1] = cur
-				EightQueens()
-				ban[cur] = false
-				// ans2[cnt+1] = 0
-
-			}
-		}
-	}
+	tryX(0)
 }
