de9c6cfdc9c1b238390b5390bf782a4707c2febe
ajaberi <adnan.ajaberi@gmail.com>
2023-03-11 17:08:22 +0300

    ye

--- point/main.go
+++ point/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 type point struct {
-	x, y int
+	x int
+	y int
+}
+
+func PrintStr(str string) {
+	arrayStr := []rune(str)
+
+	for i := range arrayStr {
+		z01.PrintRune(arrayStr[i])
+	}
 }
 
 func setPoint(ptr *point) {
@@ -11,10 +20,25 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func PrintInt(a int) {
+	r := '0'
+	if a/10 > 0 {
+		PrintInt(a / 10)
+	}
+	for i := 0; i < a%10; i++ {
+		r++
+	}
+	z01.PrintRune(r)
+}
+
 func main() {
-	points := new(point)
+	var points point
 
-	setPoint(points)
+	setPoint(&points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	PrintStr("x = ")
+	PrintInt(points.x)
+	PrintStr(", y = ")
+	PrintInt(points.y)
+	z01.PrintRune('\n')
 }
