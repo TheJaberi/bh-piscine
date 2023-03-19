567352f5ec818b82b3205cd0ef35537d3eaa5dd7
ajaberi <adnan.ajaberi@gmail.com>
2023-03-19 20:51:36 +0300

    ye

---
+++ listpushback.go
@@ -0,0 +1,11 @@
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{Data: data}
+
+	if l.Tail == nil {
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		l.Tail.Next = newNode
+		l.Tail = newNode
+	}
+}
