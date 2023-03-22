94da467132bed32f531420ebda6155b18725af00
Adnan Jaberi <ajaberi@noreply.learn.reboot01.com>
2023-03-22 06:58:30 +0000

    Ye

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -2,7 +2,7 @@ package piscine
 
 type TreeNode struct {
 	Left, Right, Parent *TreeNode
-	Data string
+	Data                string
 }
 
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
