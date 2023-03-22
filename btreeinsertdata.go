eb93dacb415f12a2da879d733061804d638956ba
ajaberi <adnan.ajaberi@gmail.com>
2023-03-23 00:52:13 +0300

    ye

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -1,29 +1,29 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
+//type TreeNode struct {
+//	Left, Right, Parent *TreeNode
+//	Data                string
+//}
 
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	node := &TreeNode{Data: data}
-	if root == nil {
-		return node
-	}
-	if data < root.Data {
-		if root.Left == nil {
-			root.Left = node
-			node.Parent = root
-		} else {
-			BTreeInsertData(root.Left, data)
-		}
-	} else if data > root.Data {
-		if root.Right == nil {
-			root.Right = node
-			node.Parent = root
-		} else {
-			BTreeInsertData(root.Right, data)
-		}
-	}
-	return root
-}
+//func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+//	node := &TreeNode{Data: data}
+//	if root == nil {
+//	return node
+//}
+//if data < root.Data {
+//	if root.Left == nil {
+//		root.Left = node
+//		node.Parent = root
+//	} else {
+//		BTreeInsertData(root.Left, data)
+//}
+//} else if data > root.Data {
+//	if root.Right == nil {
+//		root.Right = node
+//		node.Parent = root
+//	} else {
+//		BTreeInsertData(root.Right, data)
+//	}
+//}
+//return root
+//}
---
+++ btreeisbinary.go
@@ -0,0 +1,38 @@
+package piscine
+
+type TreeNode struct {
+	Data  string
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func BTreeIsBinary(root *TreeNode) bool {
+	return isSubtreeLesser(root.Left, root.Data) &&
+		isSubtreeGreater(root.Right, root.Data) &&
+		BTreeIsBinary(root.Left) &&
+		BTreeIsBinary(root.Right)
+}
+
+func isSubtreeLesser(node *TreeNode, val string) bool {
+	if node == nil {
+		return true
+	}
+	if node.Data <= val &&
+		isSubtreeLesser(node.Left, val) &&
+		isSubtreeLesser(node.Right, val) {
+		return true
+	}
+	return false
+}
+
+func isSubtreeGreater(node *TreeNode, val string) bool {
+	if node == nil {
+		return true
+	}
+	if node.Data > val &&
+		isSubtreeGreater(node.Left, val) &&
+		isSubtreeGreater(node.Right, val) {
+		return true
+	}
+	return false
+}
